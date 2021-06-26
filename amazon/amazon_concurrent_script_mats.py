28305cddd4c00f282d3bab230544efdfcce39947
unknown <kautarnahli@hotmail.es>
2021-06-26 01:08:35 +0100

    adding more features to the mat-saving in csv and json

--- amazon/amazon_concurrent_script_mats.py
+++ amazon/amazon_concurrent_script_mats.py
@@ -7,128 +7,168 @@ from time import sleep, time
 from concurrent.futures import ThreadPoolExecutor, wait
 from pathlib import Path
 
-from urllib3.packages.six import u
+from selenium import webdriver
 
-from scraper_amazon import get_driver_headless, write_to_file_Link ,write_to_csv_mat, write_to_json_mat, connect_to_base_mat
+from scraper_amazon import get_driver_headless, get_driver, write_to_file_Link ,write_to_csv_mat, write_to_json_mat, connect_to_base_mat# put_all_features_together_in_list
 from scraper_amazon import Mat_Yoga , upload_file  , upload_images
 
 #%%
 # main block
 # set variables
+def put_all_features_together_in_list(mat_url,other_features_one_mat,reviews_text_dict,table_features_dict,other_colors):
+    list_of_dict = [other_features_one_mat,reviews_text_dict,table_features_dict,other_colors]
+    all_features_keys = []
+    all_features_values = []
+    all_features_keys.append('url')
+    all_features_values.append(mat_url)
+    for i in list_of_dict:
+        for k, v in i.items():
+            all_features_keys.append(k)
+            all_features_values.append(v)
 
+    return(all_features_keys, all_features_values)
 
 def run_process_features_each_mat(mat_url, filename_csv, filename_json):
    # mat_yoga = Mat_Yoga()
     browser = get_driver_headless()
     if connect_to_base_mat(browser, mat_url):
-        sleep(5) 
+        sleep(10) 
         ## : Provider
         # 0: name
         #1,2,3 price
-        # Table Features contains: color, brand, Material,Product care, Dimentions
-
+        # table Features contains: color, brand, Material,Product care, Dimentions
 
         #table
-        table_features = {}
-        for i in range(1,7):
-            try:
-                    feature_name = browser.find_element_by_xpath('//*[@id="productOverview_feature_div"]/div/table/tbody/tr['+str(i)+']/td[1]')
-                    feature_value = browser.find_element_by_xpath('//*[@id="productOverview_feature_div"]/div/table/tbody/tr['+str(i)+']/td[2]')
-                    feature_name= feature_name.text.replace('\n', '')
-                    feature_value= feature_value.text.replace('\n', '')
-                    table_features[feature_name] = feature_value
-                    #print(feature_name, feature_value)
-            except Exception as e:
-                    feature_name = f'xpath_table{i}'
-                    feature_value = 'Nan'
-                    #print('Error P1', e)
-                    table_features[feature_name] = feature_value
-            
+        try:
+            table_features = []
+            table = browser.find_elements_by_xpath('//*[@id="productOverview_feature_div"]/div/table/tbody')
+            for i in table:
+                    table_features.append(i.text.replace('\n', ''))
+            separator = '_'
+            table_features_dict = {'table': separator.join(table_features) }  
+        except  Exception as e :
+            print('error table features', e)
+            table_features_dict = {'table': 'error' } 
         
-            print('table_feature', table_features)
+        print('table_feature', table_features_dict.keys())
      
-            #prie, name, reviews
-            all_xpath = ['//*[@id="title"]',
-            '//*[@id="priceblock_saleprice"]','//*[@id="priceblock_ourprice"]','//*[@id="price_inside_buybox"]','//*[@id="acrCustomerReviewText"]','//*[@id="acrPopover"]/span[1]/a/i[1]/span']
-            all_features_one_mat = []
-            for i in all_xpath:
-                try:
-                    feature = browser.find_element_by_xpath(i)
-                    feature_text= feature.text.replace('\n', '')
-                    all_features_one_mat.append(feature_text)
-                    #print(feature)
-                except Exception as e:
-                    feature_text = 'Nan'
-                    #print('Error P1', e)
-                    all_features_one_mat.append(feature_text)
+        #prie, name, reviews
+        other_features_one_mat = {} 
+        keys_other_paths = ['name', 'saleprice', 'ourprice', 'price_buybox','number_reviews','rating']
+        other_xpath = ['//*[@id="title"]',
+        '//*[@id="priceblock_saleprice"]','//*[@id="priceblock_ourprice"]','//*[@id="price_inside_buybox"]',
+        '//*[@id="acrCustomerReviewText"]','//*[@id="reviewsMedley"]/div/div[1]/div[2]/div[1]/div/div[2]/div/span/span']
         
-            # other colors
-            other_colors ={}
+        for k,v in zip(keys_other_paths,other_xpath):
             try:
-                price_s = browser.find_elements_by_xpath('//*[contains(@id,"_price")]/p')
-                colors= browser.find_elements_by_xpath('//*[contains(@id,"color_name_")]') 
-                #price_2 = price_1.get_attribute("innerHTML")
-                #print (len(price_1))
+                feature= browser.find_element_by_xpath(v)
+                feature_text= feature.text.replace('\n', '')
+                other_features_one_mat[k] = feature_text
+                #print(feature)
             except Exception as e:
-                    price = 'Nan'
-                    #print('Error P1', e)
-
-            for i, j in zip(colors, price_s):
-                k = i.get_attribute('title').replace('\n', '')
-                v = j.get_attribute('innerHTML').replace('\n', '')
-                other_colors[k] =v
-                    
-        
-                
+                feature_text = 'Nan'
+                print('Error other  features', e)
+                other_features_one_mat[k] = feature_text
             
+            print('other_features', other_features_one_mat.keys())
+        
 
-            # Clean price from \n
-            #all_features_one_mat= [i.replace('\n', '') for i in all_features_one_mat]
-            print('all_features_one_mat')
-            #print(all_features_one_mat)
-            write_to_csv_mat(all_features_one_mat, filename_csv, table_features, mat_url,other_colors)
-            #print('Number mat', mat_url)
-            write_to_json_mat(all_features_one_mat, filename_json,table_features, mat_url,other_colors)
-            print('writing to json mat instance')      
-            browser.quit()
+        #Reviews_text
+        try:
+            reviews_text =[]
+            path_reviews = browser.find_elements_by_xpath('//*[@id="reviewsMedley"]/div/div[2]/div/div[2]/span[3]/div/div/div[4]/div[3]')
+            for i in path_reviews:
+                reviews_text.append(i.text.replace('\n', ('|')))
+            reviews_text_dict = {'reviews_text_dict': separator.join(reviews_text)}
+
+        except Exception as e:
+            print('error reviews_text_dict',e)
+            reviews_text_dict = {'reviews_text_dict': 'error' } 
+            
+        print('reviews_text_dict', reviews_text_dict.keys())
+
+        # other colors
+        other_colors ={}
+        text_other_colors = []
+        price_colors = []
+        try:
+            #Getting block for price and color for same mat
+            other_colors_block = browser.find_elements_by_xpath('//*[contains(@id,"color_name_")]')
+            for i in other_colors_block: 
+                text_other_colors.append( i.get_attribute('innerHTML').replace('\n',''))
+                
+        except Exception as e:
+                text_other_colors = ['error']
+                print('Error other colors', e)
+
+        try:
+            #Getting block for price and color for same mat
+            other_colors_block = browser.find_elements_by_xpath('//*[contains(@id,"color_name_")]')
+            for i in other_colors_block: 
+                price_colors.append(i.find_element_by_xpath('//*[contains(@id,"price")]/p').get_attribute('innerHTML'))
+        except Exception as e:
+                price_colors = ['error']
+                print('Error other price colors', e)
+
+        other_colors['text_other_colors']= text_other_colors
+        other_colors['price_colors']= price_colors
+            
+        print('other colors',other_colors.keys())
+
+        all_keys, all_values= put_all_features_together_in_list(mat_url,other_features_one_mat,
+        reviews_text_dict,table_features_dict,other_colors)
+        sleep(5)
+        print('---------------------------------------------')
+        print(all_keys)
+       #Writing to csv is not working for now
+        write_to_csv_mat(filename_csv,all_keys, all_values)
+        print('written to csv')
+        write_to_json_mat(filename_json, all_keys, all_values )
+        print('written to json mat instance')      
+        browser.quit()
     else:
-        print("Error connecting to hacker news")
+        print("Error connecting to mat url")
         browser.quit()
 
 #%%
 
-# #%%
-# price_xpath = ["priceblock_saleprice","priceblock_ourprice","price_inside_buybox" ]
-# for i in price_xpath:
-# #     print(i)
 
+# M ='https://www.amazon.com/Friendly-Fitness-Exercise-Carrying-Exercises/dp/B08R55YR6J/ref=sr_1_217_sspa?dchild=1&keywords=yoga+mats&qid=1624447198&s=sports-and-fitness&sr=1-217-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyTE9RNTJUTDlaTERQJmVuY3J5cHRlZElkPUEwMzczODk3Mk8yNFYyT09CQTYyNCZlbmNyeXB0ZWRBZElkPUEwNzA2Njc1RDlZWTNLMzVCQjBJJndpZGdldE5hbWU9c3BfYXRmX25leHQmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+# G ='https://www.google.com/'
+# C ='https://www.amazon.com/Unbeatable-Thickness-Alignment-Carrying-Exercise/dp/B08FZNTJSZ/ref=sr_1_5113_sspa?dchild=1&keywords=yoga+mats&qid=1624456322&s=sports-and-fitness&sr=1-5113-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFGSU8wUUxaWEdCUEgmZW5jcnlwdGVkSWQ9QTA1NjY5NjUyR1RZNUdGU0FFQktMJmVuY3J5cHRlZEFkSWQ9QTAxNjY4OTMxR0tSVlhRTEFYTzZLJndpZGdldE5hbWU9c3BfYXRmX25leHQmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+# A ='https://www.amazon.com/Yoga-Direct-Deluxe-Sticky-4-Inch/dp/B007MTZFUW/ref=sr_1_222?dchild=1&keywords=yoga+mats&qid=1624447198&s=sports-and-fitness&sr=1-222'
+# B ='https://www.amazon.com/YuniMuse-Anti-Tear-Cushioning-Extra-Thick-Pink-purple/dp/B08GG7WRX9/ref=sr_1_221?dchild=1&keywords=yoga+mats&qid=1624447198&s=sports-and-fitness&sr=1-221'
+# testo =[A,B]
 # browser = get_driver_headless()
-# mat_url = 'https://www.amazon.com/Unbeatable-Thickness-Alignment-Carrying-Exercise/dp/B08FZNTJSZ/ref=sr_1_5113_sspa?dchild=1&keywords=yoga+mats&qid=1624456322&s=sports-and-fitness&sr=1-5113-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFGSU8wUUxaWEdCUEgmZW5jcnlwdGVkSWQ9QTA1NjY5NjUyR1RZNUdGU0FFQktMJmVuY3J5cHRlZEFkSWQ9QTAxNjY4OTMxR0tSVlhRTEFYTzZLJndpZGdldE5hbWU9c3BfYXRmX25leHQmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
-# if connect_to_base_mat(browser, mat_url):
-#         sleep(10) 
-   
-#         try:
-#             price_s = browser.find_elements_by_xpath('//*[contains(@id,"_price")]/p')
-#             colors= browser.find_elements_by_xpath('//*[contains(@id,"color_name_")]') 
-#             #price_2 = price_1.get_attribute("innerHTML")
-#             #print (len(price_1))
-#         except Exception as e:
-#             price = 'Nan'
-#             print('Error P1', e)
+# browser.get(A)
 
-#         for i, j in zip(colors, price_s):
-#             k = i.get_attribute('title').replace('\n', '')
-            # v = j.get_attribute('innerHTML').replace('\n', '')
-                    
-#%%
-#%%
-#s= price_1.find_elements_by_xpath('//*[contains(@id,"color_name_")]') 
-#//div[contains(@id,'test')]
-#//*[@id="color_name_1_price"]/p
-#//*[@id="color_name_0"]
-#//*[@id="color_name_0"]
-# //*[@id="color_name_0"]
-# //*[@id="a-autoid-11-announce"]/div/div[1]/img
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -162,7 +202,7 @@ if __name__ == "__main__":
 
     # crawl
     with ThreadPoolExecutor() as executor:
-        for number in (0,2):
+        for number in range (0,2):
             futures.append(
                 #executor.submit(run_process_features_each_mat, urls['url'][number], output_filename_csv, output_filename_json)
                 executor.submit(run_process_features_each_mat, testo[number], output_filename_csv, output_filename_json)
@@ -289,4 +329,29 @@ if __name__ == "__main__":
 # write_to_json_mat(l, 'test.json')
 # write_to_json_mat(l2, 'test.json')
 # 
-# %%
+# #%%
+# dict_1 = {
+#      'osama' : ['$37.99', 'Nan', '$37.99'] ,
+#      'rida' : ['$37.99', 'Nan', '$37.99'] 
+
+#     }
+# def put_all_features_together_in_list(mat_url,other_features_one_mat,reviews_text_dict,table_features_dict,other_colors):
+#     list_of_dict = [other_features_one_mat,reviews_text_dict,table_features_dict,other_colors]
+#     all_features_keys = []
+#     all_features_values = []
+#     all_features_keys.append('url')
+#     all_features_values.append(mat_url)
+#     for i in list_of_dict:
+#         for k, v in i.items():
+#             all_features_keys.append(k)
+#             all_features_values.append(v)
+
+#     return(all_features_keys, all_features_values)
+
+# a , b = put_all_features_together_in_list('a', dict_1,dict_1,dict_1,dict_1)
+
+# # %%
+# a
+# # %%
+# b
+# # %%
