caa8b767c8a4270610d6f23a31ed3795e75b1b6d
unknown <kautarnahli@hotmail.es>
2021-07-20 13:46:54 +0100

    cleaning price,next  put one singular  column for price

--- amazon/cleaning_amazon.py
+++ amazon/cleaning_amazon.py
@@ -2,12 +2,17 @@
 import pandas as pd
 import numpy as np
 from pandas.core.indexes.base import Index
+import matplotlib.pyplot as plt
+pd.options.display.max_colwidth
 # %%
 columns = ['mat_url','name','saleprice', 'ourprice', 'price_buybox','number_reviews','rating',
 'reviews_text', 'table_features_color_care_material', 'other_colors_and_prices','other_prices']
 #reading data from the 26/06/2021
-df1 = pd.read_csv('..\data_amazon\Amazon_webscraped_data_not_clean\Amazon_output_features_20210626_1.csv',header=None)
-df2 = pd.read_csv('..\data_amazon\Amazon_webscraped_data_not_clean\Amazon_output_features_20210626_2.csv',header=None)
+#df1 = pd.read_csv('..\data_amazon\Amazon_webscraped_data_not_clean\Amazon_output_features_20210626_1.csv',header=None)
+#df2 = pd.read_csv('..\data_amazon\Amazon_webscraped_data_not_clean\Amazon_output_features_20210626_2.csv',header=None)
+df1 = pd.read_csv('C:\\Users\\kautar\\Documents\\Aicore\\Projects\\Webscraping\\data_amazon\\Amazon_webscraped_data_not_clean\\Amazon_output_features_20210626_1.csv',header=None)
+df2 = pd.read_csv('C:\\Users\\kautar\\Documents\\Aicore\\Projects\\Webscraping\\data_amazon\\Amazon_webscraped_data_not_clean\\Amazon_output_features_20210626_2.csv',header=None)
+
 
 #concatenate  the two dataframes
 df = pd.concat([df1,df2])
@@ -22,24 +27,41 @@ df.shape
 #nulls not because an error 
 df.isnull().sum()
 #%%
+df.isna().sum()
+#%%
 df.info()
 # %%
 df.head()
 # %%
-#%%
 import re
-regex = re.compile(r'alt=(.*?)style')
-df['other_colors'] = df['other_colors_and_prices'].str.findall(regex).apply('-'.join)
+regex_color = re.compile(r'alt=(.*?)style')
+df['other_colors'] = df['other_colors_and_prices'].str.findall(regex_color).apply('-'.join)
+#%%
+regex_price = re.compile(r'Price(.*?)<')
+df['other_prices']=df['other_colors_and_prices'].str.findall(regex_price).apply('-'.join)
+df.drop(['other_colors_and_prices'],axis=1, inplace=True)
+
 
 # %%
 for i in columns:
-   df[i] = df[i].apply(lambda y:np.nan  if y=='[]' else y)
-   df[i] = df[i].apply(lambda y:np.nan  if y=="['error']" else y)
+    try:
+        df[i] = df[i].apply(lambda y:np.nan  if y=='[]' else y)
+        df[i] = df[i].apply(lambda y:np.nan  if y=="['error']" else y)
+    except Exception as e:
+        print(e)
 df = df.replace(r'', np.NaN)
+#%%
+
 #%%
 df.isnull().sum()
 #%%
-#price
+df.isna().sum()
+df.info()
+#%%
+df['other_prices'][2]
+df['other_prices_to fill_ourprice'] = df['other_prices'].apply(lambda x: replace_3(x)).astype('float') 
+#%%
+#cleaning all prices and putting all in one single column, converting to float
 def replace_1(x):
     if x:
         for k,v in [('$',""),("-"," ")]:
@@ -56,23 +78,61 @@ def replace_2(x):
         return x.split()[0]
     else:
         return np.nan
-         
-
+def replace_3(x):
+    if x==np.nan:
+        return np.nan
+        
+    else:
+        for k,v in [('$',""),(">","")]:
+           x = x.split()[0] 
+           x = x.replace(k,v)
+        return x
+        
+#%%        
+#cleaning saleprice
 df['saleprice'] = df['saleprice'].str.replace('$',"").astype('float')
-df['other_ourprice'] =df['ourprice'].apply(lambda x: replace_1(x)).astype('float')
-df['ourprice'] = df['ourprice'].apply(lambda x: replace_2(x)).astype('float')
+#dividing our price in two cclumns ourprice_1 and ourprice_2
+df['ourprice_2'] =df['ourprice'].apply(lambda x: replace_1(x)).astype('float')
+df['ourprice_1'] = df['ourprice'].apply(lambda x: replace_2(x)).astype('float')
+df.drop(['ourprice'],axis=1, inplace=True)
+#cleaning buybox
 df['price_buybox'] = df['price_buybox'].str.replace('$',"").astype('float')
-df['other_ourprice'].unique()
-
-
-
+#Cleaning prices from other colors
+df['other_prices_to fill_ourprice'] = df['other_prices'].apply(lambda x: replace_3(x)).astype('float') 
+#%%
+plt.plot(df['saleprice'], label='saleprice')
+plt.plot(df['ourprice_2'], label='ourprice_2')
+plt.plot(df['ourprice_1'], label='ourprice_1')
+plt.plot(df['price_buybox'], label='price_buybox')
+#%%
+#find outliers 
+df[df['ourprice_1']>200]
  # %%
-df.isna().sum()
-# %%
-df['saleprice'].unique()
+pd.options.display.max_colwidth
+#Droping outlier: price over 700
+df.loc[525, 'mat_url']
+df.drop(525,axis=0, inplace=True)
+#%%
+#only mat costing more than 200, 
+df.loc[951, 'mat_url']
 
 # %%
-df.head(300)
+plt.plot(df['saleprice'], label=f"saleprice-{df['saleprice'].isna().sum()}")
+plt.plot(df['ourprice_2'], label=f"ourprice_2-{df['ourprice_2'].isna().sum()}")
+plt.plot(df['ourprice_1'], label=f"ourprice_1-{df['ourprice_1'].isna().sum()}")
+plt.plot(df['price_buybox'], label=f"price_buybox-{df['price_buybox'].isna().sum()}")
+plt.legend()
+
+# %%
+prices = df.fillna(0)['saleprice']+df.fillna(0)['ourprice_2']+df.fillna(0)['ourprice_1']+df.fillna(0)['price_buybox']
+
+
+#%%
+bool= prices==0
+bool.sum()
+#184 missing values for the price
+df[bool]
+#%%
 #other prices and table features
 
 # %%
